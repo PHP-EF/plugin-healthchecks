6bace77dce10f4c8335329945e8d727ca0b56cf8
TehMuffinMoo <mat.cox@tmmn.uk>
2025-07-18 15:08:33 +0000

    dev

--- plugin.php
+++ plugin.php
@@ -328,9 +328,9 @@ class healthChecksPlugin extends phpef {
 										toast("Test Failed", row.name, data["data"]["error"], "danger","30000");
 									} else {
 										if (data["data"]["type"] == "web") {
-											toast("Test Failed", row.name, "HTTP Status Code: " + data["data"]["http_code"] + " - <b>Expected:</b> " + row.http_expected_status + "\n\n<b>Response: </b> " + data["data"]["response"], "danger","30000");
+											toast("Test Failed", row.name, "HTTP Status Code: " + data["data"]["http_code"] + " - <b>Expected:</b> " + row.http_expected_status + "\n\n<b>Response: </b> " + escapeHTML(data["data"]["response"]), "danger","30000");
 										} else {
-											toast("Test Failed", row.name, data["data"]["response"], "danger","30000");
+											toast("Test Failed", row.name, escapeHTML(data["data"]["response"]), "danger","30000");
 										}
 									}
 								}
